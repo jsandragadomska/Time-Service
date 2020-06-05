dcd78980c7efb719f13a386dbd389ed02bdec57f
Sandra Gadomska <gadomska.sandra@gmail.com>
2020-06-05 13:00:29 +0200

    ready to package

--- src/main/java/com/gadomska/timeservice/TimeserviceApplication.java
+++ src/main/java/com/gadomska/timeservice/TimeserviceApplication.java
@@ -2,12 +2,22 @@ package com.gadomska.timeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.GregorianCalendar;
 
 @SpringBootApplication
+@EnableScheduling
 public class TimeserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeserviceApplication.class, args);
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void printDate(){
+        System.out.println("My Time Service : " + new GregorianCalendar().getTime() + " yo!");
     }
 
 }
