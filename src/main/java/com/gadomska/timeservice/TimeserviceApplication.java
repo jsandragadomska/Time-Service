e56956e8e54470a893cd3138fb6a9c10cc162eef
Sandra Gadomska <gadomska.sandra@gmail.com>
2020-06-05 12:09:39 +0200

    initial commit

---
+++ src/main/java/com/gadomska/timeservice/TimeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gadomska.timeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gadomska/timeservice/TimeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadomska.timeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
